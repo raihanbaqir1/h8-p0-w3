bb1c8619f83661d213b1b0809f85b0e4003706bb
Raihan <raihanbaqir@gmail.com>
2019-02-06 17:49:24 +0700

    change exercises8

--- exercises8.js
+++ exercises8.js
@@ -1,13 +1,19 @@
 function pasanganTerbesar(num) {
     var numString = num.toString();
-    numString = numString.split(",");
-    
-    return 
-  }
+    var terbesar = 0;
+    for(var i = 0; i < numString.length; i++){
+        var tampung = numString.substr(i,2);
+        if(tampung > terbesar){
+            terbesar = tampung;
+        }
+    }
+    terbesar = Number(terbesar);
+    return terbesar;
+}
   
   // TEST CASES
   console.log(pasanganTerbesar(641573)); // 73
-//   console.log(pasanganTerbesar(12783456)); // 83
-//   console.log(pasanganTerbesar(910233)); // 91
-//   console.log(pasanganTerbesar(71856421)); // 85
-//   console.log(pasanganTerbesar(79918293)); // 99
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
