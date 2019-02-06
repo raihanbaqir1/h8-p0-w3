8780eb1072774e8781f23afb878f54a37e36bb0a
Raihan <raihanbaqir@gmail.com>
2019-02-06 07:02:52 +0700

    change exercises4

--- exercises4.js
+++ exercises4.js
@@ -1,4 +1,10 @@
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
+// input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
+// ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
 function dataHandling2(input){
-
+    input.splice(1,2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung")
+    input.splice(4,1, "Pria", "SMA Internasional Metro")
+    console.log(input)
+    input.split()
+     
 }
+dataHandling2(["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"])
