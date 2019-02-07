c04a3cff3be9f1f2ab8686cf341011748a51ed05
Raihan <raihanbaqir@gmail.com>
2019-02-07 07:52:43 +0700

    change exercises10

--- exercises10.js
+++ exercises10.js
@@ -1,10 +1,22 @@
+// var arr = [2,4,6]
 function perkalianUnik(arr) {
+    var multiply = 1
+    for(var i = 0; i < arr.length; i++){
+        multiply = multiply * arr[i];
+    }
+    var result = [];
+    for(var i = 0; i < arr.length; i++){
+        result2 = Math.round(multiply/arr[i]);
+        result.push(result2)
+        
+    }
+    return result;
     
-  }
+}
   
   // TEST CASES
   console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
-//   console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
-//   console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
-//   console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
-//   console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
+  console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+  console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+  console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+  console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
