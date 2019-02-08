159af137ee0529173e6bbcc16db954e32ed18c58
Raihan <raihanbaqir@gmail.com>
2019-02-08 14:18:32 +0700

    change exercises15

--- exercises13.js
+++ exercises13.js
@@ -12,11 +12,12 @@ function targetTerdekat(arr) {
         }    
     }
     // return counter;
-  }
+}
+// KURANG DINAMIS
   
   // TEST CASES
-  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
-  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
-  console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-  console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+//   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+//   console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+//   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+//   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+//   console.log(targetTerdekat([' ', ' ', ' ', 'x', 'x', ' ', 'o', 'x'])); // 2
--- exercises15.js
+++ exercises15.js
@@ -1,9 +1,29 @@
 function groupAnimals(animals) {
+    var alfabet = "abcdefghijklmnopqrstuvwxyz";
     var result = [];
-    for(var i = 0; i <= animals.length; i++){
+    var array1 = [];
 
+    for(var i = 0; i <= alfabet.length; i++){
+        array1 = [];
+        for(j = 0; j < animals.length; j++){
+            if(alfabet[i]===animals[j][0]){
+                // array1 = [];
+                array1.push(animals[j]);
+            //   console.log(array1)  
+            }
+        }
+        if(array1.length!==0){
+            result.push(array1);
+        }
     }
-  }
+    return result;
+}
+
+        
+        
+            
+        
+
   
   // TEST CASES
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
