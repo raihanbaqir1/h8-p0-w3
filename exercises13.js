7eacffb4cfcc0c60960d0f5c8c618e6002e116c5
Raihan <raihanbaqir@gmail.com>
2019-02-08 14:42:38 +0700

    change exercises13

--- exercises13.js
+++ exercises13.js
@@ -16,8 +16,8 @@ function targetTerdekat(arr) {
 // KURANG DINAMIS
   
   // TEST CASES
-//   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
-//   console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-//   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
-//   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-//   console.log(targetTerdekat([' ', ' ', ' ', 'x', 'x', ' ', 'o', 'x'])); // 2
+  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+  console.log(targetTerdekat([' ',  'o', ' ', 'x', 'x', ' ', , 'x'])); // 2
